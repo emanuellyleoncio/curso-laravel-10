60fa4c71670b86304fec92315b19389ab58cc0fc
Emanuelly Leoncio <emanuelly.leoncio.ext@infracommerce.com.br>
2023-12-02 10:17:20 -0300

    feat: Model and Migration of Support

--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,1 +1,21 @@
-<h1>Listagem</h1>
+<h1>Listagem dos Suportes</h1>
+
+
+<table>
+    <thead>
+        <th>Assunto</th>
+        <th>Status</th>
+        <th>Descrição</th>
+        <th></th>
+    </thead>
+
+    <tbody>
+        @foreach($supports as $support)
+            <tr><td>{{ $support->subject }}</td></tr>
+            <tr><td>{{ $support->status }}</td></tr>
+            <tr><td>{{ $support->body }}</td></tr>
+            <tr><td>></td></tr>
+        @endforeach
+    </tbody>
+</table>
+
